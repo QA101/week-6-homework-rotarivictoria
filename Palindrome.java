379979504a9367edb647bda3f47dc93dc53f1a56
rotarivictoria <rotarivictoria@yahoo.com>
2017-12-02 12:37:53 -0800

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+
+public class Palindrome {
+
+	
+		  public static void checkPalindrome(String s)
+		  {
+		    
+		    String reverse = new StringBuffer(s).reverse().toString();
+		 
+		    
+		    if (s.equals(reverse))
+		      System.out.println("Yes");
+		 
+		    else
+		      System.out.println("No");
+		  }
+		 
+		  public static void main (String[] args)
+		               throws java.lang.Exception
+		  {
+		    checkPalindrome("toyota");
+		    checkPalindrome("civic");
+		  
+		
+	}
+
+}
